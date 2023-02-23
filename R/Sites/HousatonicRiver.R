c8275aec32103333daf817dfe17393227f962698
martie <andres-martinez@uiowa.edu>
2023-02-22 19:16:52 -0600

    Start changing code. Need to remove sites and include USGS data

--- R/Sites/HousatonicRiver.R
+++ R/Sites/HousatonicRiver.R
@@ -1,7 +1,7 @@
 ## Water PCB concentrations data analysis per site
-# Housatonic River
-# Aroclors 1254 and 1260
-# GE facility map @https://semspub.epa.gov/work/01/574882.pdf
+## Housatonic River
+## Aroclors 1254 and 1260, no congener analysis
+## GE facility map @https://semspub.epa.gov/work/01/574882.pdf
 
 # Install packages
 install.packages("tidyverse")
@@ -15,110 +15,87 @@ install.packages("MuMIn")
 install.packages("lmerTest")
 install.packages("zoo")
 install.packages("dataRetrieval")
+install.packages("reshape")
 
 # Load libraries
-library(ggplot2)
-library(scales) # function trans_breaks
-library(stringr) # str_detect
-library(robustbase) # function colMedians
-library(dplyr) # performs %>%
-library(tibble) # adds a column
-library(lme4) # performs lme
-library(MuMIn) # gets Rs from lme
-library(lmerTest) # gets the p-value from lme
-library(zoo) # yields seasons
-library(dataRetrieval) # read data from USGS
+{
+  library(ggplot2)
+  library(scales) # function trans_breaks
+  library(stringr) # str_detect
+  library(robustbase) # function colMedians
+  library(dplyr) # performs %>%
+  library(tibble) # adds a column
+  library(lme4) # performs lme
+  library(MuMIn) # gets Rs from lme
+  library(lmerTest) # gets the p-value from lme
+  library(zoo) # yields seasons
+  library(dataRetrieval) # read data from USGS
+  library(reshape)
+}
 
 # Read data ---------------------------------------------------------------
 # Data in pg/L
-wdc <- read.csv("WaterDataCongenerAroclor08052022.csv")
+wdc <- read.csv("Data/WaterDataCongenerAroclor08052022.csv")
 
 # Select Housatonic River data ---------------------------------------------------
-hou <- wdc[str_detect(wdc$SiteName, 'Housatonic River'),]
+hou.0 <- wdc[str_detect(wdc$LocationName, 'Housatonic River'),]
 
 # Data preparation --------------------------------------------------------
-# Calculate total PCB
-tpcb.hou <- rowSums(hou[, c(14:117)], na.rm = T)
-# Change date format
-hou$SampleDate <- as.Date(hou$SampleDate, format = "%m/%d/%y")
-# Calculate sampling time
-time.day <- data.frame(as.Date(hou$SampleDate) - min(as.Date(hou$SampleDate)))
-# Create individual code for each site sampled
-site.numb <- hou$LocationID %>% as.factor() %>% as.numeric
-# Include season
-yq.s <- as.yearqtr(as.yearmon(hou$SampleDate, "%m/%d/%Y") + 1/12)
-season.s <- factor(format(yq.s, "%q"), levels = 1:4,
-                   labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
-# Create data frame
-hou.tpcb <- cbind(factor(hou$LocationID), hou$SampleDate,
-                  hou$Latitude, hou$Longitude, as.matrix(tpcb.hou),
-                  data.frame(time.day), site.numb, season.s)
-# Add column names
-colnames(hou.tpcb) <- c("location", "date", "Latitude", "Longitude",
-                        "tPCB", "time", "site.code", "season")
+{
+  # Remove samples (rows) with total PCBs  = 0
+  hou.1 <- hou.0[!(rowSums(hou.0[, c(14:117)], na.rm = TRUE)==0),]
+  # Calculate total PCB
+  tpcb.hou <- rowSums(hou.1[, c(14:117)], na.rm = T)
+  # Change date format
+  hou.1$SampleDate <- as.Date(hou.1$SampleDate, format = "%m/%d/%y")
+  # Calculate sampling time
+  time.day <- data.frame(as.Date(hou.1$SampleDate) - min(as.Date(hou.1$SampleDate)))
+  # Create individual code for each site sampled
+  site.numb <- hou.1$SiteID %>% as.factor() %>% as.numeric
+  # Include season
+  yq.s <- as.yearqtr(as.yearmon(hou.1$SampleDate, "%m/%d/%Y") + 1/12)
+  season.s <- factor(format(yq.s, "%q"), levels = 1:4,
+                     labels = c("0", "S-1", "S-2", "S-3")) # winter, spring, summer, fall
+  # Create data frame
+  hou.tpcb <- cbind(factor(hou.1$SiteID), hou.1$SampleDate,
+                    hou.1$Latitude, hou.1$Longitude, as.matrix(tpcb.hou),
+                    data.frame(time.day), site.numb, season.s)
+  # Add column names
+  colnames(hou.tpcb) <- c("SiteID", "date", "Latitude", "Longitude",
+                          "tPCB", "time", "site.code", "season")
+}
 
 # Get coordinates per site to plot in Google Earth
-hou.location <- hou.tpcb[c('location', 'Latitude', 'Longitude', 'tPCB')]
+hou.location <- hou.tpcb[c('SiteID', 'Latitude', 'Longitude', 'tPCB')]
 # Average tPCB per site
-hou.location <- aggregate(tPCB ~ location + Latitude + Longitude,
-                            data = hou.location, mean)
-
-# (2) Calculate total log PCB
-# Remove metadata
-hou.log <- subset(hou, select = -c(SampleID:AroclorCongener))
-# Remove Aroclor data
-hou.log <- subset(hou.log, select = -c(A1016:A1260))
-# Log 10 individual PCBs 
-hou.log <- log10(hou.log)
-# Replace -inf to NA
-hou.log <- do.call(data.frame,
-                     lapply(hou.log,
-                            function(x) replace(x, is.infinite(x), NA)))
-# Sum individual log 10 PCBs
-hou.log.tpcb <- rowSums(hou.log, na.rm = T)
-# Generate data.frame for analysis and plots
-hou.log.tpcb <- cbind(factor(hou$LocationID), hou$SampleDate,
-                      as.matrix(hou.log.tpcb), data.frame(time.day),
-                      site.numb, season.s)
-colnames(hou.log.tpcb) <- c("location", "date", "logtPCB", "time",
-                            "site.code", "season")
+hou.location <- aggregate(tPCB ~ SiteID + Latitude + Longitude,
+                          data = hou.location, mean)
 
 # General plots -------------------------------------------------------------------
 # (1) Histograms
-# (1.1) tPCB
 hist(hou.tpcb$tPCB)
 hist(log10(hou.tpcb$tPCB))
-# (1.2) log.tPCB
-hist(hou.log.tpcb$logtPCB)
-hist(log10(hou.log.tpcb$logtPCB))
 
 # (2) Time trend plots
-# (2.1) tPCB
 ggplot(hou.tpcb, aes(y = tPCB,
                      x = format(date,'%Y'))) +
-  geom_point() +
+  geom_point(shape = 21, size = 2, fill = "#66ccff") +
   xlab("") +
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   theme_bw() +
   theme(aspect.ratio = 5/15) +
+  ylab(expression(bold(atop("Water Concetration",
+                            paste(Sigma*"PCB (pg/L)"))))) +
+  theme(axis.text.y = element_text(face = "bold", size = 9),
+        axis.title.y = element_text(face = "bold", size = 10)) +
   theme(axis.text.x = element_text(face = "bold", size = 9,
-                                   angle = 60, hjust = 1,
-                                   color = "black"))
-
-# (2.2) log.tPCB
-ggplot(hou.log.tpcb, aes(y = logtPCB,
-                         x = format(date,'%Y'))) +
-  geom_point() +
-  xlab("") +
-  theme_bw() +
-  theme(aspect.ratio = 5/15) +
-  theme(axis.text.x = element_text(face = "bold", size = 9,
-                                   angle = 60, hjust = 1,
-                                   color = "black"))
+                                   angle = 60, hjust = 1),
+        axis.title.x = element_text(face = "bold", size = 9)) +
+  annotate("text", x = 21, y = 10^5.6, label = "Housotonic River",
+           size = 4)
 
 # (3) Seasonality
-# (3.1) tPCB
 ggplot(hou.tpcb, aes(x = season, y = tPCB)) +
   xlab("") +
   scale_x_discrete(labels = c("winter", "spring", "summer", "fall")) +
@@ -126,78 +103,44 @@ ggplot(hou.tpcb, aes(x = season, y = tPCB)) +
                 labels = trans_format("log10", math_format(10^.x))) +
   theme_bw() +
   theme(aspect.ratio = 5/15) +
-  ylab(expression(bold("Water Conncetration " *Sigma*"PCB 2012 - 2018 (pg/L)"))) +
+  ylab(expression(bold(atop("Water Concetration",
+                            paste(Sigma*"PCB (pg/L)"))))) +
   theme(axis.text.y = element_text(face = "bold", size = 9),
         axis.title.y = element_text(face = "bold", size = 9)) +
-  theme(axis.text.x = element_text(face = "bold", size = 8,
+  theme(axis.text.x = element_text(face = "bold", size = 9,
                                    angle = 60, hjust = 1),
         axis.title.x = element_text(face = "bold", size = 8)) +
-  theme(axis.ticks = element_line(size = 0.8, color = "black"), 
+  theme(axis.ticks = element_line(linewidth = 0.8, color = "black"), 
         axis.ticks.length = unit(0.2, "cm")) +
   annotation_logticks(sides = "l") +
   geom_jitter(position = position_jitter(0.3), cex = 1.2,
-              shape = 1, col = "#66ccff") +
-  geom_boxplot(width = 0.7, outlier.shape = NA, alpha = 0)
-
-# (3.2) log.tPCB
-ggplot(hou.log.tpcb, aes(x = season, y = logtPCB)) +
-  xlab("") +
-  scale_x_discrete(labels = c("winter", "spring", "summer", "fall")) +
-  theme_bw() +
-  theme(aspect.ratio = 5/15) +
-  ylab(expression(bold("Water Conncetration " *Sigma*"PCB 2012 - 2018 (pg/L)"))) +
-  theme(axis.text.y = element_text(face = "bold", size = 9),
-        axis.title.y = element_text(face = "bold", size = 9)) +
-  theme(axis.text.x = element_text(face = "bold", size = 8,
-                                   angle = 60, hjust = 1),
-        axis.title.x = element_text(face = "bold", size = 8)) +
-  theme(axis.ticks = element_line(size = 0.8, color = "black"), 
-        axis.ticks.length = unit(0.2, "cm")) +
-  annotation_logticks(sides = "l") +
-  geom_jitter(position = position_jitter(0.3), cex = 1.2,
-              shape = 1, col = "#66ccff") +
-  geom_boxplot(width = 0.7, outlier.shape = NA, alpha = 0)
+              shape = 21, fill = "#66ccff") +
+  geom_boxplot(width = 0.7, outlier.shape = NA, alpha = 0) +
+  annotate("text", x = 3.5, y = 10^5.6, label = "Housotonic River",
+           size = 4)
 
 # (4) Sites
-# (4.1) tPCB
-ggplot(hou.tpcb, aes(x = factor(location), y = tPCB,
-                     color = location)) + 
+ggplot(hou.tpcb, aes(x = factor(SiteID), y = tPCB)) + 
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   theme_bw() +
   xlab(expression("")) +
   theme(aspect.ratio = 5/20) +
-  ylab(expression(bold("Water Conncetration " *Sigma*"PCB 2012 - 2018 (pg/L)"))) +
+  ylab(expression(bold(atop("Water Concetration",
+                            paste(Sigma*"PCB (pg/L)"))))) +
   theme(axis.text.y = element_text(face = "bold", size = 9),
         axis.title.y = element_text(face = "bold", size = 9)) +
   theme(axis.text.x = element_text(face = "bold", size = 8,
                                    angle = 60, hjust = 1),
         axis.title.x = element_text(face = "bold", size = 8)) +
-  theme(axis.ticks = element_line(size = 0.8, color = "black"), 
+  theme(axis.ticks = element_line(linewidth = 0.8, color = "black"), 
         axis.ticks.length = unit(0.2, "cm")) +
   annotation_logticks(sides = "l") +
   geom_jitter(position = position_jitter(0.3), cex = 1.2,
-              shape = 1) +
-  geom_boxplot(width = 0.7, outlier.shape = NA, alpha = 0, color = "black") +
-  theme(legend.position = "none")
-
-# (4.2) log.tPCB
-ggplot(hou.log.tpcb, aes(x = factor(location), y = logtPCB)) + 
-  theme_bw() +
-  xlab(expression("")) +
-  theme(aspect.ratio = 5/20) +
-  ylab(expression(bold("Water Conncetration " *Sigma*"PCB 2012 - 2018 (pg/L)"))) +
-  theme(axis.text.y = element_text(face = "bold", size = 9),
-        axis.title.y = element_text(face = "bold", size = 9)) +
-  theme(axis.text.x = element_text(face = "bold", size = 8,
-                                   angle = 60, hjust = 1),
-        axis.title.x = element_text(face = "bold", size = 8)) +
-  theme(axis.ticks = element_line(size = 0.8, color = "black"), 
-        axis.ticks.length = unit(0.2, "cm")) +
-  annotation_logticks(sides = "l") +
-  geom_jitter(position = position_jitter(0.3), cex = 1.2,
-              shape = 1, col = "#66ccff") +
-  geom_boxplot(width = 0.7, outlier.shape = NA, alpha = 0)
+              shape = 21, fill = "#66ccff") +
+  geom_boxplot(width = 0.7, outlier.shape = NA, alpha = 0) +
+  annotate("text", x = 30, y = 10^5.6, label = "Housotonic River",
+           size = 3)
 
 # Include USGS flow data --------------------------------------------------
 # Include flow data from USGS station Housatonic River
@@ -222,131 +165,7 @@ hou.log.tpcb$flow.1 <- 0.03*flow.1$X_00060_00003[match(hou.log.tpcb$date,
 hou.log.tpcb$flow.2 <- 0.03*flow.2$X_00060_00003[match(hou.log.tpcb$date,
                                                   flow.2$Date)]
 
-# Regressions -------------------------------------------------------------
-# (1) All sites ---------------------------------------------------------------
-# (1.1) Perform linear regression (lr)
-# (1.1.1) tPCB vs. time
-lr.hou.tpcb.t <- lm(log10(tPCB) ~ time, data = hou.tpcb)
-# See results
-summary(lr.hou.tpcb.t)
-# Look at residuals
-res <- resid(lr.hou.tpcb.t) # get list of residuals
-# Create Q-Q plot for residuals
-qqnorm(res)
-# Add a straight diagonal line to the plot
-qqline(res)
-# Shapiro test
-shapiro.test(res)
-# One-sample Kolmogorov-Smirnov test
-ks.test(res, 'pnorm')
 
-# (1.1.2) log.tPCB vs. time
-lr.hou.log.tpcb.t <- lm(logtPCB ~ time, data = hou.log.tpcb)
-# See results
-summary(lr.hou.log.tpcb.t)
-# Look at residuals
-res <- resid(lr.hou.log.tpcb.t) # get list of residuals
-# Create Q-Q plot for residuals
-qqnorm(res)
-# Add a straight diagonal line to the plot
-qqline(res)
-# Shapiro test
-shapiro.test(res)
-# One-sample Kolmogorov-Smirnov test
-ks.test(res, 'pnorm')
-
-# (1.1.3) tPCB vs. season
-lr.hou.tpcb.s <- lm(log10(tPCB) ~ season, data = hou.tpcb)
-# See results
-summary(lr.hou.tpcb.s)
-# Look at residuals
-res <- resid(lr.hou.tpcb.s) # get list of residuals
-# Create Q-Q plot for residuals
-qqnorm(res)
-# Add a straight diagonal line to the plot
-qqline(res)
-# Shapiro test
-shapiro.test(res)
-# One-sample Kolmogorov-Smirnov test
-ks.test(res, 'pnorm')
-
-# (1.1.4) log.tPCB vs. season
-lr.hou.log.tpcb.s <- lm(logtPCB ~ season, data = hou.log.tpcb)
-# See results
-summary(lr.hou.log.tpcb.s)
-# Look at residuals
-res <- resid(lr.hou.log.tpcb.s) # get list of residuals
-# Create Q-Q plot for residuals
-qqnorm(res)
-# Add a straight diagonal line to the plot
-qqline(res)
-# Shapiro test
-shapiro.test(res)
-# One-sample Kolmogorov-Smirnov test
-ks.test(res, 'pnorm')
-
-# (1.1.5) tPCB vs. flow
-lr.hou.tpcb.f <- lm(log10(tPCB) ~ flow.1, data = hou.tpcb)
-# See results
-summary(lr.hou.tpcb.f)
-# Look at residuals
-res <- resid(lr.hou.tpcb.f) # get list of residuals
-# Create Q-Q plot for residuals
-qqnorm(res)
-# Add a straight diagonal line to the plot
-qqline(res)
-# Shapiro test
-shapiro.test(res)
-# One-sample Kolmogorov-Smirnov test
-ks.test(res, 'pnorm')
-
-# (1.1.6) log.tPCB vs. flow
-lr.hou.log.tpcb.f <- lm(logtPCB ~ flow.1, data = hou.log.tpcb)
-# See results
-summary(lr.hou.log.tpcb.f)
-# Look at residuals
-res <- resid(lr.hou.log.tpcb.f) # get list of residuals
-# Create Q-Q plot for residuals
-qqnorm(res)
-# Add a straight diagonal line to the plot
-qqline(res)
-# Shapiro test
-shapiro.test(res)
-# One-sample Kolmogorov-Smirnov test
-ks.test(res, 'pnorm')
-
-# (1.2) MLR
-# (1.2.1) tPCB vs. time + season + flow
-mlr.hou.tpcb <- lm(log10(tPCB) ~ time + season + flow.1,
-                   data = hou.tpcb)
-# See results
-summary(mlr.hou.tpcb)
-# Look at residuals
-res <- resid(mlr.hou.tpcb) # get list of residuals
-# Create Q-Q plot for residuals
-qqnorm(res)
-# Add a straight diagonal line to the plot
-qqline(res)
-# Shapiro test
-shapiro.test(res)
-# One-sample Kolmogorov-Smirnov test
-ks.test(res, 'pnorm')
-
-# (1.2.2) log.tPCB vs. time + season + flow
-mlr.hou.log.tpcb <- lm(logtPCB ~ time + season + flow.1,
-                       data = hou.log.tpcb)
-# See results
-summary(mlr.hou.log.tpcb)
-# Look at residuals
-res <- resid(mlr.hou.log.tpcb) # get list of residuals
-# Create Q-Q plot for residuals
-qqnorm(res)
-# Add a straight diagonal line to the plot
-qqline(res)
-# Shapiro test
-shapiro.test(res)
-# One-sample Kolmogorov-Smirnov test
-ks.test(res, 'pnorm')
 
 # (1.3) Perform Linear Mixed-Effects Model (LMEM)
 # (1.3.1) tPCB vs. time + season + flow + site
