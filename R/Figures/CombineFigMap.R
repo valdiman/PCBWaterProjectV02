9b0277149abe45fd1a01a48a50f81ae461ae7746
martie <andres-martinez@uiowa.edu>
2023-08-17 16:20:27 -0500

    working, but still not perfect.

--- R/Figures/CombineFigMap.R
+++ R/Figures/CombineFigMap.R
@@ -1,21 +1,26 @@
-library(cowplot)
-library(grid)
+
+library(ggplot2)
 library(png)
-library(gridExtra)
 
-# Read the first plot from a folder and convert to ggplot-compatible object
-map <- rasterGrob(readPNG("Output/Maps/Sites/maptPCBFoxRiverAveV01.png"))
+# Read the PNG images
+map <- readPNG("Output/Maps/Sites/maptPCBFoxRiverAveV01.png")
+plot <- readPNG("Output/Plots/Sites/Temporal/plotFoxRiverTimeV01.png")
 
-# Read the second plot from another folder and convert to ggplot-compatible object
-plot <- rasterGrob(readPNG("Output/Plots/Sites/Temporal/plotFoxRiverTimeV01.png"))
+# Create a plot with a smaller canvas
+p <- ggplot() +
+  xlim(0, 6) + ylim(0, 6) +  # Adjust the limits as needed
+  theme_void()
 
-# Create a grid arrangement with the map and plot positioned next to each other
-grid_arrange <- arrangeGrob(
-  map, plot,
-  layout_matrix = matrix(c(1, 2), ncol = 2, byrow = TRUE),
-  widths = unit(c(0.7, 0.3), c("npc", "npc")),
-  heights = unit(c(1), "null")
-)
+# Add the images at specific positions with reduced size
+p <- p +
+  annotation_custom(rasterGrob(map),
+                    xmin = 0, xmax = 5, ymin = 1, ymax = 6) +
+  annotation_custom(rasterGrob(plot, width = unit(1.5*.8, "in"), height = unit(0.75*.8, "in")),
+                    xmin = 3.3, xmax = 5.3, ymin = 1.8, ymax = 3.8)
 
-# Display or save the grid arrangement
-grid.arrange(grid_arrange)
+# Print the plot
+print(p)
+
+# Save plot in folder
+ggsave("Output/Figures/Sites/FoxRiverV01.png",
+       plot = p, width = 6, height = 4, dpi = 300)
